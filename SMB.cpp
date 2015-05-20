1c54a937da87375996702e4e275565d2472fd6ff
Mitchell Sternke <mitchellsternke@gmail.com>
2015-05-20 01:50:30 -0500

    Implemented MoveAllSpritesOffscreen and MoveSpritesOffscreen from the disassembly

--- SMB.cpp
+++ SMB.cpp
@@ -8,7 +8,7 @@
 
 /// Macro for creating a memory address variable
 #define BIND( func, address ) SMB_TranslationTable[address] = func
-#define MEMORY( name, address ) MemoryAccess name(address)
+#define MEMORY( name, address ) static MemoryAccess name(address)
 
 /*
  * Static variables
@@ -20,9 +20,16 @@ SMB_Function SMB_TranslationTable[65536];
  * Memory Address Locations
  */
 
+MEMORY( Sprite_Y_Position, 0x0200 );
+
 /*
  * Functions
  */
+
+static void InitializeMemory();
+static void MoveAllSpritesOffscreen();
+static void MoveSpritesOffscreen();
+static void MoveSpritesOffscreenRoutine(byte);
 
 void InitializeMemory()
 {
@@ -50,6 +57,30 @@ void InitializeMemory()
 	writeY(y);
 }
 
+void MoveAllSpritesOffscreen()
+{
+	// Move all sprites offscreen
+	MoveSpritesOffscreenRoutine(0);
+}
+
+void MoveSpritesOffscreen()
+{
+	// Move all but sprite 0 offscreen
+	MoveSpritesOffscreenRoutine(4);
+}
+
+void MoveSpritesOffscreenRoutine( byte start )
+{
+	// Set the y value of sprites to 248
+	byte i = start;
+	do
+	{
+		Sprite_Y_Position[i] = 0xf8;
+		i += 4; // Each entry is stored 4 bytes apart
+	}
+	while( i != 0 );
+}
+
 /*
  * Public functions defined in SMB.hpp
  */
@@ -63,5 +94,7 @@ void SMB_Init()
 	}
 
 	// Function binding points
-	BIND( InitializeMemory, 0x90cc );
+	BIND( InitializeMemory,        0x90cc );
+	BIND( MoveAllSpritesOffscreen, 0x8220 );
+	BIND( MoveSpritesOffscreen,    0x8223 );
 }
